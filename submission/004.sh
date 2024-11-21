489cfb1d9371116efe974a694b0f659c3032d21f
Rogério Ferreira <rogeriob2br@gmail.com>
2024-11-21 12:56:59 -0300

    Update 004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -1,7 +1,9 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
-descriptor="tr('xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2'/100)"
-descriptor_info=$(bitcoin-cli getdescriptorinfo $descriptor)
-descriptor_with_checksum=$(echo $descriptor_info | jq -r '.descriptor')
+
+xpub="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
+descriptor="tr(${xpub}/100)"
+descriptor_info=$(bitcoin-cli getdescriptorinfo "$descriptor")
+descriptor_with_checksum=$(echo "$descriptor_info" | jq -r '.descriptor')
 address=$(bitcoin-cli deriveaddresses "$descriptor_with_checksum" | jq -r '.[0]')
 echo "$address"
