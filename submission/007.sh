96c7328e162cba5fec5edbba113d5ae2f81b3cfd
Rogério Ferreira <rogeriob2br@gmail.com>
2024-11-21 14:49:07 -0300

    Update 007.sh

--- submission/007.sh
+++ submission/007.sh
@@ -1,1 +1,22 @@
 # Only one single output remains unspent from block 123,321. What address was it sent to?
+block_hash=$(bitcoin-cli getblockhash 123321)
+
+txs=$(bitcoin-cli getblock $block_hash | jq -r '.tx[]')
+
+declare -a unspent_outputs
+
+for txid in $txs; do
+    raw_tx=$(bitcoin-cli getrawtransaction $txid)
+    decoded_tx=$(bitcoin-cli decoderawtransaction $raw_tx)
+    for vout in $(echo $decoded_tx | jq -r '.vout | keys[]'); do
+        is_unspent=$(bitcoin-cli gettxout $txid $vout)
+        if [ ! -z "$is_unspent" ]; then
+            address=$(echo $decoded_tx | jq -r ".vout[$vout].scriptPubKey.address")
+            unspent_outputs+=("$address")
+        fi
+    done
+done
+for address in "${unspent_outputs[@]}"; do
+    echo "$address"
+done
+
